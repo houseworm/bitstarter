9edc5759b5199d2533be49fdc65041f91f01aef5
Ubuntu <ubuntu@ip-172-31-41-38.us-west-2.compute.internal>
2013-07-14 04:02:54 +0000

    Worked version for HW3 submission part 2

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env nodeOAAOAOA
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "https://github.com/houseworm/bitstarter/blob/0d0b18c840030453c803cc9c7f8b7f23d8176e71/index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,6 +46,45 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
+
+var checkHtmlURL= function(url, savedhtmlfile, checksfile){
+    restler.get(url).on('complete', function(result){
+	if (result instanceof Error){
+	    this.retry(5000);
+	}else{
+	    fs.writeFile(savedhtmlfile, result, function(err){
+		if (err) throw err;
+	    });
+	    fs.exists(savedhtmlfile, function(exists){
+		//console.log("file exists = " + exists.toString()); 
+		if(exists){
+		    //var checkJson = checkHtmlBuffer(result, checksfile);
+		    var checkJson = checkHtmlFile(savedhtmlfile, checksfile); 
+		    var outJson = JSON.stringify(checkJson, null, 4); 
+
+		    console.log(outJson);
+
+
+		}//else{
+		    //this.retry(5000); 
+		//}    
+    
+	    })	
+
+	}
+    });
+};
+
+var checkHtmlBuffer = function(htmlbuffer, checksfile){
+    $ = cheerio.load(htmlbuffer);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
@@ -65,10 +106,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option ('-u, --url <url_to_file>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (typeof program.url !== 'undefined'){
+	//console.log("jump in URL CASE?"); 
+	//var checkJson = 
+	checkHtmlURL(program.url, "indextest.html", program.checks);
+	//json check and console log write for outJson was included in checkHtmlURL
+	//as a workaround to avoid undefined buffer issue 
+   
+    } else if (typeof program.file !== 'undefined'){
+	//console.log("jump in FILE CASE?"); 
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4); 
+
+	console.log(outJson);
+    }	
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
